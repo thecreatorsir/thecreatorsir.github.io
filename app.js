132a4b72a9866929a0d4fb91fd811153ed88589e
thecreatorsir <56694038+thecreatorsir@users.noreply.github.com>
2020-01-26 15:41:11 +0530

    ...

---
+++ app.js
@@ -0,0 +1,8 @@
+$(".port-item").click(function() {
+  $(".collapse").collapse("hide");
+});
+
+$(document).on("click", '[data-toggle="lightbox"]', function(e) {
+  e.preventDefault();
+  $(this).ekkoLightbox();
+});
